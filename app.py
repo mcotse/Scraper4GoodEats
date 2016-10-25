e2e5f27db42d188b08731ce7514260c6be212ae0
Matthew Tse <mtse9779@gmail.com>
2016-10-25 01:13:00 -0400

    fetch files and save to /results

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import bs4 as bs
 import urllib
+import requests
+import shutil
 
 #make the soup
 sauce = urllib.urlopen('http://www.seriouseats.com/2016/10/the-food-lab-how-to-bake-bacon-in-the-oven.html').read()
 soup = bs.BeautifulSoup(sauce,'lxml')
 
 #get all the img links
-img_links = []
+img_urls = {}
 for img in soup.find_all('img'):
-    link = img.attrs.get('data-src','')
-    name = link.rsplit('/',1)[-1]
-    img_links.append({name:link}) if link else None
+    url = img.attrs.get('data-src','')
+    name = url.rsplit('/',1)[-1]
+    if url:
+        img_urls[name]=url
 
-print img_links
+# print img_urls
+for name,url in img_urls.iteritems():
+    response = requests.get(url, stream=True)
+    with open('results/'+name, 'wb') as out_file:
+        print 'getting file: ',name
+        shutil.copyfileobj(response.raw, out_file)
+    del response
