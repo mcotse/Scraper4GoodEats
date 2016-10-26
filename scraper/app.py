ca564c3342c760d4e36771ba558621df0f12e220
Matthew Tse <mtse9779@gmail.com>
2016-10-26 00:59:30 -0400

    take command line argument for scraping url

--- scraper/app.py
+++ scraper/app.py
@@ -1,6 +1,11 @@
 import util
+import sys
 from multiprocessing import Pool
 
-links = util.get_all_links('http://www.seriouseats.com/user/profile/Goodeaterkenji',20)
+url = sys.argv[0]
+#how many pages to extend
+extend_page = sys.argv[1]
+
+links = util.get_all_links(sys.argv[0],sys.argv[1])
 p = Pool(4)
 p.map(util.get_all_images,links)
